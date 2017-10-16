f2e3603f8ccc696e88ca9ac7d368a2a21a95b10a
Vaishnavi Mishra <31037720+geekgeekvm@users.noreply.github.com>
2017-10-16 09:15:09 +0530

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -5,11 +5,11 @@ symbollist = ["aapl","amzn", "goog","baba"]
 i = 0
 for symbol in symbollist:
     url = "http://finance.yahoo.com/q?s="+symbollist[i]+"&q1=1"
-    htmlfile = urllib.urlopen(url)
-    htmltext = htmlfile.read()
+    hfile = urllib.urlopen(url)
+    htext = hfile.read()
 
     regex = '<span id="yfs_l84_' + symbollist[i] + '">(.+?)</span>'
     pattern = re.compile(regex)
-    price = re.findall(pattern,htmltext)
+    price = re.findall(pattern,htext)
     print price
     i+=1
